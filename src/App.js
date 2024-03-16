cdefde7968c958bbf60d72fb0df868fbf66b87d1
abimbola-ai <aojikutu20@aiip.alueducation.com>
2024-03-16 11:39:59 -0400

    new setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './components/homepage/LandingPage'
+import Auth0ProviderWithHistory from './Auth0ProviderWithHistory'
+// import WelcomePage from './components/welcome/WelcomePage'
+import { createBrowserHistory } from 'history'
 
-function App() {
+const history = createBrowserHistory()
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Auth0ProviderWithHistory history={history}>
+        <div className="container-fluid">
+          <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/user" element={<WelcomePage />} />
+          </Routes>
+        </div>
+      </Auth0ProviderWithHistory>
+    </Router>
+  )
 }
 
-export default App;
+export default App
